1b30560080e114203da6270016fa7025d304a368
Firegame224 <xg474627@gmail.com>
2026-03-08 22:36:07 +0700

    refactor: enhance sitemap.xml for better SEO

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -44,7 +44,7 @@ export const Route = createFileRoute('/sitemap.xml')({
 
                 return new Response(xml, {
                     headers: {
-                        "Content-Type": "application/xml",
+                        "Content-Type": "application/xml; charset=utf-8",
                         "Cache-Control": "public, max-age=3600"
                     }
                 })
