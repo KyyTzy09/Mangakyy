9754dafa9ef5184e1f52bf55cde303db57124961
Firegame224 <xg474627@gmail.com>
2026-03-08 23:23:49 +0700

    fix(vite.config): disable link crawling in tanstackStart prerender settings

--- vite.config.ts
+++ vite.config.ts
@@ -14,12 +14,7 @@ const config = defineConfig({
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: false,
-      },
-    }),
+    tanstackStart(),
     viteReact(),
   ],
 })
